a1da7b51c0624c2021f50291339c4142efcb434e
Joe Guo <joe.guo@canonical.com>
2021-04-20 21:16:23 +1200

    bootstrap: install espeak
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y python3-dev python3-pip libzbar-dev
+sudo apt install -y python3-dev python3-pip libzbar-dev espeak
 sudo python3 -m pip install -U pip opencv-python pyzbar
