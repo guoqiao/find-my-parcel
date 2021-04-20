a934b2b417100e9b2f357552d27f919787910d1d
Joe Guo <joe.guo@canonical.com>
2021-04-20 21:21:17 +1200

    install v4l-utils
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y python3-dev python3-pip libzbar-dev espeak
+sudo apt install -y python3-dev python3-pip libzbar-dev espeak v4l-utils
 sudo python3 -m pip install -U pip opencv-python pyzbar
